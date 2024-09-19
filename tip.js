ab84e301609a0650c53d1f52a9e7b1a30130fa09
mrprikshit <prikshitdesigner@gmail.com>
2024-09-19 12:13:53 +0530

    Add files via upload

---
+++ tip.js
@@ -0,0 +1,23 @@
+function select(selector) {
+    let elem = document.querySelector(selector);
+    let tip = document.createElement("span");
+    tip.className = "tipjs-tip"
+    elem.appendChild(tip);
+    elem.text=(text)=> {
+        tip.innerText = text;
+    }
+    elem.danger=()=>{
+        tip.classList.add("danger");
+    }
+    elem.addEventListener("mousemove", (tipevent)=> {
+        let x = 5+tipevent.clientX;
+        let y = 20+tipevent.clientY;
+        tip.style.top = y+"px";
+        tip.style.left = x+"px";
+        tip.classList.add("show-tipjs-tip");
+    })
+    elem.addEventListener("mouseout", ()=> {
+        tip.classList.remove("show-tipjs-tip");
+    })
+    return elem
+}
